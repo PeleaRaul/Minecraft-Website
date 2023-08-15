6e034066d08ef841b0d635e36201b1cb91280703
Pelea Raul-Daniel <118268304+PeleaRaul@users.noreply.github.com>
2023-08-15 11:58:13 +0100

    Animation Optimization

--- assets/animation.js
+++ assets/animation.js
@@ -1,75 +1,85 @@
-var canvas; var context; var screenH; var screenW; var stars = []; var fps = 60; var numStars = 500;
+var canvas;
+var context;
+var screenH;
+var screenW;
+var stars = [];
+var fps = 60;
+var numStars = 500;
+var animateInterval;
 
 $('document').ready(function() {
-	screenH = $(window).height();
-	screenW = $(window).width();
-	canvas = $('#space');
-	canvas.attr('height', screenH);
-	canvas.attr('width', screenW);
-	context = canvas[0].getContext('2d');
-	for(var i = 0; i < numStars; i++) {
-		var x = Math.round(Math.random() * screenW);
-		var y = Math.round(Math.random() * screenH);
-		var length = 1 + Math.random() * 1.5;
-		var opacity = Math.random();
-		var star = new Star(x, y, length, opacity);
-		stars.push(star);
-	}
-	animateInterval = setInterval(animate, 1000 / fps);
+  screenH = $(window).height();
+  screenW = $(window).width();
+  canvas = $('#space')[0];
+  canvas.height = screenH;
+  canvas.width = screenW;
+  context = canvas.getContext('2d');
+  var i;
+  for (i = 0; i < numStars; i++) {
+    var x = Math.round(Math.random() * screenW);
+    var y = Math.round(Math.random() * screenH);
+    var length = 1 + Math.random() * 1.5;
+    var opacity = Math.random();
+    var star = new Star(x, y, length, opacity);
+    stars.push(star);
+  }
+  animate();
 });
+
 function animate() {
-	context.clearRect(0, 0, screenW, screenH);
-	$.each(stars, function() {
-		this.draw(context);
-	})
-}
-function stopAnimation()
-{
-     clearInterval(animateInterval);
-}
-function Star(x, y, length, opacity) {
-	this.x = parseInt(x);
-	this.y = parseInt(y);
-	this.length = parseInt(length);
-	this.opacity = opacity;
-	this.factor = 1;
-	this.increment = Math.random() * .03;
-}
-Star.prototype.draw = function() {
-	context.rotate((Math.PI * 1 / 10));
-	context.save();
-	context.translate(this.x, this.y);
-	if(this.opacity > 1) {
-		this.factor = -1;
-	}
-	else if(this.opacity <= 0) {
-		this.factor = 1;
-		
-		this.x = Math.round(Math.random() * screenW);
-		this.y = Math.round(Math.random() * screenH);
-	}
-		
-	this.opacity += this.increment * this.factor;
-	
-	context.beginPath()
-	for (var i = 5; i--;) {
-		context.lineTo(0, this.length);
-		context.translate(0, this.length);
-		context.rotate((Math.PI * 2 / 10));
-		context.lineTo(0, - this.length);
-		context.translate(0, - this.length);
-		context.rotate(-(Math.PI * 6 / 10));
-	}
-	context.lineTo(0, this.length);
-	context.closePath();
-	context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
-	context.shadowBlur = 5;
-	context.shadowColor = '#fff';
-	context.fill();
-	
-	context.restore();
+  context.clearRect(0, 0, screenW, screenH);
+  stars.forEach(function(star) {
+    star.draw(context);
+  });
+  requestAnimationFrame(animate);
 }
 
+function stopAnimation() {
+  cancelAnimationFrame(animateInterval);
+}
+
+function Star(x, y, length, opacity) {
+  this.x = parseInt(x);
+  this.y = parseInt(y);
+  this.length = parseInt(length);
+  this.opacity = opacity;
+  this.factor = 1;
+  this.increment = Math.random() * .03;
+}
+
+Star.prototype.draw = function() {
+  context.save();
+  context.translate(this.x, this.y);
+
+  if (this.opacity > 1) {
+    this.factor = -1;
+  } else if (this.opacity <= 0) {
+    this.factor = 1;
+    this.x = Math.round(Math.random() * screenW);
+    this.y = Math.round(Math.random() * screenH);
+  }
+
+  this.opacity += this.increment * this.factor;
+
+  context.beginPath();
+  context.moveTo(0, this.length);
+  for (var i = 5; i--;) {
+    context.lineTo(0, this.length);
+    context.translate(0, this.length);
+    context.rotate((Math.PI * 2 / 10));
+    context.lineTo(0, -this.length);
+    context.translate(0, -this.length);
+    context.rotate(-(Math.PI * 6 / 10));
+  }
+  context.closePath();
+
+  context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
+  context.shadowBlur = 5;
+  context.shadowColor = '#fff';
+  context.fill();
+
+  context.restore();
+};
   /* 
 © 2023 AppoDev's Minecraft Server Template. All rights reserved.
 
