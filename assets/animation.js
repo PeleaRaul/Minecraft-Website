673bef71d08a347ac2cad208cf5813c8540df9bc
Pelea Raul-Daniel <118268304+PeleaRaul@users.noreply.github.com>
2023-08-15 12:01:09 +0100

    Update animation.js

--- assets/animation.js
+++ assets/animation.js
@@ -1,11 +1,11 @@
-var canvas;
-var context;
-var screenH;
-var screenW;
-var stars = [];
-var fps = 60;
-var numStars = 500;
-var animateInterval;
+let canvas;
+let context;
+let screenH;
+let screenW;
+const stars = [];
+const fps = 60;
+const numStars = 500;
+let animateInterval;
 
 $('document').ready(function() {
   screenH = $(window).height();
@@ -14,23 +14,26 @@ $('document').ready(function() {
   canvas.height = screenH;
   canvas.width = screenW;
   context = canvas.getContext('2d');
-  var i;
-  for (i = 0; i < numStars; i++) {
-    var x = Math.round(Math.random() * screenW);
-    var y = Math.round(Math.random() * screenH);
-    var length = 1 + Math.random() * 1.5;
-    var opacity = Math.random();
-    var star = new Star(x, y, length, opacity);
+
+  for (let i = 0; i < numStars; i++) {
+    const x = Math.round(Math.random() * screenW);
+    const y = Math.round(Math.random() * screenH);
+    const length = 1 + Math.random() * 1.5;
+    const opacity = Math.random();
+    const star = new Star(x, y, length, opacity);
     stars.push(star);
   }
+
   animate();
 });
 
 function animate() {
   context.clearRect(0, 0, screenW, screenH);
-  stars.forEach(function(star) {
-    star.draw(context);
-  });
+
+  for (let i = 0, len = stars.length; i < len; i++) {
+    stars[i].draw(context);
+  }
+
   requestAnimationFrame(animate);
 }
 
@@ -47,7 +50,7 @@ function Star(x, y, length, opacity) {
   this.increment = Math.random() * .03;
 }
 
-Star.prototype.draw = function() {
+Star.prototype.draw = function(context) {
   context.save();
   context.translate(this.x, this.y);
 
@@ -63,7 +66,8 @@ Star.prototype.draw = function() {
 
   context.beginPath();
   context.moveTo(0, this.length);
-  for (var i = 5; i--;) {
+
+  for (let i = 5; i--;) {
     context.lineTo(0, this.length);
     context.translate(0, this.length);
     context.rotate((Math.PI * 2 / 10));
@@ -71,6 +75,7 @@ Star.prototype.draw = function() {
     context.translate(0, -this.length);
     context.rotate(-(Math.PI * 6 / 10));
   }
+
   context.closePath();
 
   context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
@@ -80,37 +85,3 @@ Star.prototype.draw = function() {
 
   context.restore();
 };
-  /* 
-© 2023 AppoDev's Minecraft Server Template. All rights reserved.
-
-Statement/Declaration of Rights
-
-Subject to the provisions of this notice, this website and all its content, information,
-or material is the copyright of AppoDev's Minecraft Server Template, together with its licensors. Accordingly,
-your use of our website or its services does not constitute any license to use the copyright in our website.
-
-Except to the extent permitted by the applicable copyright law or AppoDev's Minecraft Server Template, 
-any form of use, reproduction, or redistribution of part of all of the content, information, or material
-on this website in any form is hereby prohibited without license or permission.
-
-You may not, except otherwise with prior permission and express written consent by the AppoDev's Minecraft Server Template, 
-copy, download, print, extract, exploit, adapt, edit, modify, republish, reproduce, rebroadcast, duplicate, distribute, 
-or publicly display any of the content, information, or material on this website for non-personal or commercial purposes, 
-except for any other use as permitted by the applicable copyright law.
-
-You may also not transmit, host, or store any such content, information, or material in any form or through any means, 
-including but not limited to photocopying, recording, or in any print, electronic or digital form, or on any other website.
-
-However, you are permitted to copy the content, information, or material on this website for personal use, 
-educational use of individual third parties, government use, or any other use permitted under the applicable 
-copyright law while acknowledging this author AppoDev's Minecraft Server Template as the source of any such content, information, or material.
-
-Subject to our terms & conditions, you are not permitted to post information or add content copyrighted by third parties. 
-Pursuant to the Digital Millennium Copyright Act (DMCA), AppoDev's Minecraft Server Template will respond to takedown notices 
-or abuse reports by copyright owners to remove any abusive or infringing content on this website.
-
-AppoDev's Minecraft Server Template will not be responsible for the quality, accuracy, completeness,
-or appropriateness of the content, information, or material on this website.
-
-Removing any copyright claim or "this data" from the code, you may face applicable copyright laws!
-*/
